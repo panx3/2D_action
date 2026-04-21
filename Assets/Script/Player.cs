b5788563de0b5d24cb26d8b5a3bc07f659b12e2d
aoriikanosuke <aoriika1122@icloud.com>
2026-04-21 12:04:20 +0900

    add pan-kun animation

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,14 +5,20 @@ public class Player : MonoBehaviour
 {
     [SerializeField, Header("移動速度")]
     private float _moveSpeed;
+    [SerializeField, Header("ジャンプ速度")]
+    private float _jumpSpeed;
 
     private Vector2 _inputDirection;
     private Rigidbody2D _rigid;
+    private bool _bjump;
+    private Animator _anim;
 
    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rigid = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        _bjump = false;
     }
 
     // Update is called once per frame
@@ -23,9 +29,23 @@ public class Player : MonoBehaviour
 
     private void  _Move(){
         _rigid.linearVelocity = new Vector2(_inputDirection.x * _moveSpeed, _rigid.linearVelocity.y);
+        _anim.SetBool("Walk", _inputDirection.x != 0.0f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.tag == "Floor"){
+            _bjump = false;
+        }
     }
 
     public void _OnMove(InputAction.CallbackContext context){
         _inputDirection = context.ReadValue<Vector2>();
+    }
+
+    public void _OnJump(InputAction.CallbackContext context){
+        if(!context.performed || _bjump) return;
+
+        _rigid.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+        _bjump = true;
     }
 }
